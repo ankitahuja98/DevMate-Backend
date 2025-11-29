4c27933ec582f9f173169ea1a14c7d75e9d255a0
Ankit <ankitahuja80@gmail.com>
2025-11-30 04:02:54 +0530

    feed Api created

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const userRouter = express.Router();
 
@@ -54,6 +55,50 @@ userRouter.get("/user/matches", userAuth, async (req, res) => {
       success: true,
       data: matches,
     });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Something went Wrong",
+    });
+  }
+});
+
+// Get all user -- feed API
+userRouter.get("/feed", userAuth, async (req, res) => {
+  //   #swagger.tags = ["User"];
+  //   #swagger.summary = "Get Feed;
+  //   #swagger.description = "This endpoint is used for get feed, all the other user profile";
+  try {
+    const loggedInUserId = req.user._id;
+
+    // find the user with whome you connectied either you send the req or they send the req to you
+    const requests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUserId }, // you send the req
+        {
+          // they send the req to you
+          toUserId: loggedInUserId,
+          status: { $ne: ["interested"] },
+        },
+      ],
+    }).select("fromUserId toUserId");
+
+    // Convert excluded user IDs into a set
+    const excludedUserList = new Set();
+
+    requests.forEach((val) => {
+      excludedUserList.add(val.fromUserId.toString());
+      excludedUserList.add(val.toUserId.toString());
+    });
+
+    // Also exclude self
+    excludedUserList.add(loggedInUserId.toString());
+
+    const feedUsers = await User.find({
+      _id: { $nin: Array.from(excludedUserList) },
+    }).select("name age");
+
+    res.status(200).json({ data: feedUsers });
   } catch (error) {
     return res.status(500).json({
       success: false,
