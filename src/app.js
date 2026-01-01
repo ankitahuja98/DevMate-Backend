65d8ffbf583cf14299cb7b11e72ac391d0d17ec4
Ankit <ankitahuja80@gmail.com>
2026-01-01 23:53:48 +0530

    Change cors settings

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use("/devmate.api.docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // CORS Handling
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://192.168.1.7:5173"], // Devmate frontend URL
+    origin: true, // allow same-origin
     credentials: true, // if sending cookies / tokens
   })
 );
